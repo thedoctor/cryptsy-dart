69dcad0e81866d4c91dc78cd9ce8916c9dfab1ff
Matt Smith <matt.the.smith@gmail.com>
2015-04-19 15:17:04 -0700

    Initial Commit

---
+++ lib/cryptsy.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, Matt Smith. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// A Cryptsy API v2 dart library.
+///
+/// This is an awesome library. More dartdocs go here.
+library cryptsy;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/cryptsy_base.dart';
